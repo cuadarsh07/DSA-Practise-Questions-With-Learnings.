2bbbd5d58bd7e28742d49c1162dd37ee0b1dae5e
Adarsh Kumar <cuadarsh07@gmail.com>
2026-06-09 17:31:10 +0530

    Implement maxTotalValue method for subarray calculation

---
+++ Max total subarray value one.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long maxTotalValue(int[] nums, int k) {
+        int mn = nums[0];
+        int mx = nums[0];
+
+        for (int x : nums) {
+            mn = Math.min(mn, x);
+            mx = Math.max(mx, x);
+        }
+
+        return 1L * (mx - mn) * k;
+    }
+}
