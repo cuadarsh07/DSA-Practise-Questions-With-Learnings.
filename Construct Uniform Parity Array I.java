80fe1efbf833e55f37412442c0d583cfda11a801
Adarsh Kumar <cuadarsh07@gmail.com>
2026-09-02 17:00:32 +0530

    Add Solution class with uniformArray method

---
+++ Construct Uniform Parity Array I.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // It is always mathematically possible to make all elements odd 
+        // if there is a mix of parities, or keep them the same if they aren't.
+        return true;
+    }
+}
